02cf015874188d9b4eadc2211fb0b4926bb11e56
Yuri_Windows_Desktop <07yuri@gmail.com>
2022-04-26 14:03:44 -0400

    update character counter feature

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,17 @@
+let charLeft = 140;
 $(function() { //ensuring DOM has been loaded
-  console.log("DOM has loaded");
+  const $tweet = $(".new-tweet form textarea"); // Assigning the selector to a variable so the js only needs to search for the handle once
+  $tweet.on("input", function() {
+    $(this).val(function(index, value) {
+      charLeft = 140 - value.length;
+      const counter = $(this).parent().find(".counter");
+      if (charLeft < 0) { //adding CSS class of red color if the character count goes over limit
+        counter.addClass("red");
+      } else {
+        counter.removeClass("red");
+      }
+      counter.text(charLeft);
+      return value;
+    });
+  });
 });
